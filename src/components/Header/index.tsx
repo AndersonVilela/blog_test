0dba8563f14b8cdd915c1db50acd339a97ecbb8e
AndersonVilela <andersonvilela.al@gmail.com>
2023-03-27 08:52:50 -0300

    feat: add react-icons and create my header page

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import style from './style.module.css'
+import { RxHamburgerMenu } from 'react-icons/rx'
+
+export default function Header() {
+
+  const [toggleMenu, setToggleMenu] = useState(true);
+
+  const toggleMode = () => {
+    setToggleMenu(!toggleMenu);
+  }
+
+  return (
+    <>
+      <RxHamburgerMenu className={style.burg} onClick={toggleMode} />
+      <nav className={toggleMenu ? style.navContainer : style.navContainerMobile}>
+        <h3 ><a href="/">Alka Blog</a></h3>
+        <ul className={style.links}>
+          <li>Services</li>
+          <li>Blog</li>
+          <li>About us</li>
+        </ul>
+        <button>Contact us</button>
+      </nav>
+    </>
+  )
+}
