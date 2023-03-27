9dccfcfee611aabf6e8435bf3c6c88036c2adf95
AndersonVilela <andersonvilela.al@gmail.com>
2023-03-27 08:49:21 -0300

    feat: add my PostService and methods

---
+++ src/services/PostService.ts
@@ -0,0 +1,54 @@
+import HttpClient from "../config/HttpClient";
+import Post from "../models/Post";
+import CommentService from "./CommentService";
+
+export default class PostService {
+    private httpClient: HttpClient;
+
+    constructor() {
+        this.httpClient = new HttpClient();
+    }
+
+    public getAll(): Promise<Post[]> {
+        return this.httpClient.get<{
+            userId: number,
+            id: number,
+            title: string,
+            body: string
+        }[]>('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => {
+                return response.data.map((post) => {
+                    return new Post(
+                        String(post.id),
+                        post.title,
+                        post.body,
+                        String(post.userId)
+                    )
+                })
+            })
+    }
+
+    public async getOne(id: string): Promise<Post> {
+        try {
+            const comments = await new CommentService().getAll(id);
+            const { data: post } = await this.httpClient.get<{
+                userId: number,
+                id: number,
+                title: string,
+                body: string
+            }>(`https://jsonplaceholder.typicode.com/posts/${id}`)
+
+            return new Post(
+                String(post.id),
+                post.title,
+                post.body,
+                String(post.userId),
+                comments
+            )
+
+        } catch (error) {
+            throw error;
+        }
+
+    }
+}
