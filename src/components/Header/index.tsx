3dfe17dcf24605526aac0aebd88f8a0df19e72bd
AndersonVilela <andersonvilela.al@gmail.com>
2023-03-27 14:48:07 -0300

    fix: error import not usad in demo deploy

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import style from './style.module.css'
 import { RxHamburgerMenu } from 'react-icons/rx'
-import { Link } from 'react-router-dom';
 
 export default function Header() {
 
